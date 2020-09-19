507cf6a79c32cd97424d03057d372313730cbf28
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-09-19 00:36:13 -0300

    refactor: improving logic

--- src/utils/treat.ts
+++ src/utils/treat.ts
@@ -1,7 +1,7 @@
 export const treatYear = (month: number, year: number): number => {
-  return month >= 12 ? year + 1 : month <= -1 ? year - 1 : year;
+  return month > 11 ? year + 1 : month < 0 ? year - 1 : year;
 };
 
 export const treatMonth = (month: number): number => {
-  return month >= 12 ? 0 : month <= -1 ? 12 : month;
+  return month > 11 ? 0 : month < 0 ? 11 : month;
 };
